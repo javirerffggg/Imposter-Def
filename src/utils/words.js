cd5183eb53c0d730285007238ff33047c8169071
javirerffggg <160790690+javirerffggg@users.noreply.github.com>
2025-11-04 00:20:32 +0100

    Create words.js

---
+++ src/utils/words.js
@@ -0,0 +1,255 @@
+export const wordBank = {
+  animales: {
+    easy: [
+      'perro', 'gato', 'león', 'elefante', 'jirafa', 'tigre', 'oso', 'conejo',
+      'pez', 'pájaro', 'caballo', 'vaca', 'cerdo', 'oveja', 'pollo', 'pato',
+      'ratón', 'serpiente', 'rana', 'mono', 'cebra', 'cocodrilo', 'tortuga', 
+      'delfín', 'ballena', 'tiburón', 'pingüino', 'canguro', 'koala', 'panda'
+    ],
+    medium: [
+      'hipopótamo', 'rinoceronte', 'guepardo', 'leopardo', 'gorila', 'orangután',
+      'lemur', 'suricata', 'mapache', 'zorro', 'lobo', 'búho', 'águila', 'halcón',
+      'cóndor', 'flamenco', 'pelícano', 'cigüeña', 'murciélago', 'ardilla',
+      'castor', 'nutria', 'foca', 'morsa', 'orca', 'manatí', 'medusa', 'pulpo',
+      'calamar', 'estrella de mar', 'erizo de mar', 'cangrejo', 'langosta'
+    ],
+    hard: [
+      'ornitorrinco', 'equidna', 'pangolín', 'okapi', 'tapir', 'capibara',
+      'axolote', 'ajolote', 'nautilo', 'casuario', 'kiwi', 'quetzal', 'tucán',
+      'aye-aye', 'tarsero', 'loris', 'binturong', 'fosa', 'numbat', 'quokka',
+      'wombat', 'dingo', 'tasmanian devil', 'basilisco', 'dragón de komodo',
+      'monstruo de gila', 'salamandra gigante', 'esturión', 'celacanto'
+    ]
+  },
+  
+  profesiones: {
+    easy: [
+      'médico', 'profesor', 'policía', 'bombero', 'cocinero', 'camarero',
+      'conductor', 'piloto', 'enfermero', 'dentista', 'veterinario', 'cartero',
+      'panadero', 'carnicero', 'pescador', 'granjero', 'jardinero', 'limpiador',
+      'peluquero', 'fotógrafo', 'pintor', 'músico', 'cantante', 'bailarín'
+    ],
+    medium: [
+      'arquitecto', 'ingeniero', 'programador', 'diseñador', 'periodista',
+      'abogado', 'contador', 'economista', 'psicólogo', 'fisioterapeuta',
+      'farmacéutico', 'químico', 'biólogo', 'geólogo', 'astrónomo', 'arqueólogo',
+      'traductor', 'bibliotecario', 'archivista', 'editor', 'publicista',
+      'relaciones públicas', 'recursos humanos', 'asesor financiero'
+    ],
+    hard: [
+      'actuario', 'epidemiólogo', 'neurocirujano', 'forense', 'radiólogo',
+      'oncólogo', 'genetista', 'bioinformático', 'cripto-analista', 'hacker ético',
+      'sommelier', 'enólogo', 'gemólogo', 'taxidermista', 'restaurador de arte',
+      'paleontólogo', 'etnógrafo', 'lexicógrafo', 'cartógrafo', 'topógrafo',
+      'actuario de seguros', 'corredor de bolsa', 'agente de aduanas'
+    ]
+  },
+
+  países: {
+    easy: [
+      'España', 'Francia', 'Italia', 'Alemania', 'Inglaterra', 'Portugal',
+      'Estados Unidos', 'México', 'Argentina', 'Brasil', 'Chile', 'Colombia',
+      'Perú', 'Venezuela', 'China', 'Japón', 'India', 'Rusia', 'Canadá',
+      'Australia', 'Egipto', 'Marruecos', 'Sudáfrica', 'Grecia', 'Turquía'
+    ],
+    medium: [
+      'Noruega', 'Suecia', 'Finlandia', 'Dinamarca', 'Islandia', 'Irlanda',
+      'Bélgica', 'Holanda', 'Austria', 'Suiza', 'Polonia', 'República Checa',
+      'Hungría', 'Rumanía', 'Bulgaria', 'Croacia', 'Serbia', 'Ucrania',
+      'Tailandia', 'Vietnam', 'Indonesia', 'Filipinas', 'Malasia', 'Singapur',
+      'Corea del Sur', 'Israel', 'Emiratos Árabes', 'Arabia Saudí', 'Irán'
+    ],
+    hard: [
+      'Liechtenstein', 'Luxemburgo', 'Mónaco', 'Andorra', 'San Marino', 'Maldivas',
+      'Bután', 'Nepal', 'Laos', 'Camboya', 'Myanmar', 'Bangladesh', 'Sri Lanka',
+      'Kazajistán', 'Uzbekistán', 'Kirguistán', 'Tayikistán', 'Turkmenistán',
+      'Georgia', 'Armenia', 'Azerbaiyán', 'Montenegro', 'Macedonia del Norte',
+      'Bosnia y Herzegovina', 'Albania', 'Moldavia', 'Eslovenia', 'Eslovaquia'
+    ]
+  },
+
+  comida: {
+    easy: [
+      'pizza', 'hamburguesa', 'pasta', 'arroz', 'pan', 'queso', 'jamón',
+      'pollo', 'pescado', 'ensalada', 'sopa', 'tortilla', 'bocadillo',
+      'patatas fritas', 'helado', 'chocolate', 'galletas', 'tarta', 'manzana',
+      'plátano', 'naranja', 'fresa', 'sandía', 'melón', 'uvas', 'tomate'
+    ],
+    medium: [
+      'sushi', 'tacos', 'burritos', 'paella', 'risotto', 'lasaña', 'ravioli',
+      'gnocchi', 'ceviche', 'empanadas', 'arepas', 'tamales', 'falafel',
+      'hummus', 'tzatziki', 'moussaka', 'ratatouille', 'bouillabaisse',
+      'croissant', 'crepe', 'waffle', 'pancake', 'brownie', 'cheesecake',
+      'tiramisu', 'panna cotta', 'cannoli', 'baklava', 'churros'
+    ],
+    hard: [
+      'foie gras', 'escargot', 'caviar', 'trufa', 'anguila ahumada',
+      'pulpo a la gallega', 'rabo de toro', 'callos', 'mollejas', 'criadillas',
+      'percebes', 'zamburiñas', 'navajas', 'gulas', 'kokotxas', 'txangurro',
+      'gyoza', 'tempura', 'teriyaki', 'yakitori', 'ramen', 'udon', 'bibimbap',
+      'kimchi', 'pad thai', 'tom yum', 'rendang', 'nasi goreng', 'laksa'
+    ]
+  },
+
+  deportes: {
+    easy: [
+      'fútbol', 'baloncesto', 'tenis', 'natación', 'atletismo', 'ciclismo',
+      'voleibol', 'balonmano', 'golf', 'esquí', 'patinaje', 'surf', 'boxeo',
+      'karate', 'judo', 'gimnasia', 'ping pong', 'badminton', 'hockey',
+      'rugby', 'béisbol', 'softbol', 'waterpolo', 'equitación', 'vela'
+    ],
+    medium: [
+      'esgrima', 'taekwondo', 'lucha libre', 'halterofilia', 'remo', 'piragüismo',
+      'tiro con arco', 'tiro olímpico', 'pentatlón', 'triatlón', 'biatlón',
+      'bobsleigh', 'skeleton', 'luge', 'curling', 'snowboard', 'freestyle',
+      'skateboarding', 'escalada deportiva', 'bmx', 'mountain bike', 'motocross',
+      'rallies', 'fórmula 1', 'motogp', 'karting', 'parkour', 'crossfit'
+    ],
+    hard: [
+      'sepak takraw', 'kabaddi', 'korfball', 'lacrosse', 'polo', 'críquet',
+      'squash', 'racquetball', 'jai alai', 'petanca', 'bochas', 'croquet',
+      'ultimate frisbee', 'disc golf', 'footgolf', 'padel', 'pickleball',
+      'tchoukball', 'bossaball', 'chess boxing', 'zorb football', 'quidditch',
+      'orientación', 'geocaching', 'parkour freerunning', 'slackline', 'kitesurf'
+    ]
+  },
+
+  películas: {
+    easy: [
+      'Titanic', 'Avatar', 'Frozen', 'El Rey León', 'Toy Story', 'Buscando a Nemo',
+      'Star Wars', 'Harry Potter', 'El Señor de los Anillos', 'Jurassic Park',
+      'Indiana Jones', 'Piratas del Caribe', 'Spider-Man', 'Batman', 'Superman',
+      'Iron Man', 'Los Vengadores', 'Gladiador', 'Matrix', 'Terminator'
+    ],
+    medium: [
+      'El Padrino', 'Pulp Fiction', 'Forrest Gump', 'Cadena Perpetua',
+      'El Club de la Lucha', 'Origen', 'Interstellar', 'El Caballero Oscuro',
+      'Django Desencadenado', 'Bastardos sin Gloria', 'Reservoir Dogs',
+      'Kill Bill', 'El Resplandor', 'Psicosis', 'Alien', 'Blade Runner',
+      'Regreso al Futuro', 'E.T.', 'Tiburón', 'Rocky', 'Taxi Driver'
+    ],
+    hard: [
+      'El Ciudadano Kane', 'Casablanca', 'Vértigo', 'La Dolce Vita', '8½',
+      'Rashomon', 'Los Siete Samurái', 'Persona', 'El Séptimo Sello',
+      'Andrei Rublev', 'Stalker', 'Solaris', 'El Espejo', 'Metrópolis',
+      'El Gabinete del Doctor Caligari', 'Nosferatu', 'M, el Vampiro de Düsseldorf',
+      'La Naranja Mecánica', '2001: Una Odisea del Espacio', 'Barry Lyndon'
+    ]
+  },
+
+  música: {
+    easy: [
+      'guitarra', 'piano', 'batería', 'flauta', 'violín', 'trompeta',
+      'saxofón', 'bajo', 'armónica', 'acordeón', 'tambor', 'maracas',
+      'rock', 'pop', 'jazz', 'blues', 'reggae', 'hip hop', 'rap', 'metal',
+      'punk', 'country', 'folk', 'salsa', 'bachata', 'merengue'
+    ],
+    medium: [
+      'clarinete', 'oboe', 'fagot', 'trompa', 'trombón', 'tuba', 'arpa',
+      'cello', 'viola', 'contrabajo', 'banjo', 'ukelele', 'mandolina',
+      'funk', 'soul', 'disco', 'techno', 'house', 'trance', 'dubstep',
+      'drum and bass', 'reggaeton', 'cumbia', 'tango', 'flamenco', 'bossa nova'
+    ],
+    hard: [
+      'clavicordio', 'clavecín', 'theremin', 'hang drum', 'sitar', 'koto',
+      'shamisen', 'erhu', 'didgeridoo', 'gaita', 'zanfona', 'laúd', 'vihuela',
+      'krautrock', 'shoegaze', 'post-rock', 'math rock', 'djent', 'vaporwave',
+      'witch house', 'chillwave', 'lo-fi hip hop', 'trap latino', 'dembow',
+      'grime', 'drill', 'afrobeat', 'highlife', 'soukous'
+    ]
+  },
+
+  tecnología: {
+    easy: [
+      'ordenador', 'teléfono', 'tablet', 'televisión', 'radio', 'cámara',
+      'impresora', 'ratón', 'teclado', 'pantalla', 'altavoz', 'micrófono',
+      'internet', 'wifi', 'email', 'aplicación', 'web', 'red social',
+      'videojuego', 'consola', 'USB', 'disco duro', 'batería', 'cargador'
+    ],
+    medium: [
+      'router', 'servidor', 'base de datos', 'algoritmo', 'inteligencia artificial',
+      'realidad virtual', 'realidad aumentada', 'blockchain', 'criptomoneda',
+      'nube', 'streaming', 'podcast', 'smartwatch', 'dron', 'impresora 3D',
+      'robot', 'domótica', 'IoT', 'GPS', 'NFC', 'bluetooth', 'fibra óptica',
+      'LED', 'OLED', 'procesador', 'tarjeta gráfica', 'RAM', 'SSD'
+    ],
+    hard: [
+      'machine learning', 'deep learning', 'neural network', 'quantum computing',
+      'edge computing', 'fog computing', 'container', 'kubernetes', 'docker',
+      'microservicio', 'API REST', 'GraphQL', 'WebAssembly', 'Progressive Web App',
+      'serverless', 'DevOps', 'CI/CD', 'gitops', 'infraestructura como código',
+      'mesh network', 'software defined network', 'zero trust', 'homomorphic encryption',
+      'federated learning', 'GAN', 'transformer', 'BERT', 'GPT', 'diffusion model'
+    ]
+  },
+
+  objetos: {
+    easy: [
+      'mesa', 'silla', 'puerta', 'ventana', 'cama', 'sofá', 'armario', 'espejo',
+      'lámpara', 'libro', 'bolígrafo', 'lápiz', 'papel', 'tijeras', 'goma',
+      'reloj', 'gafas', 'sombrero', 'zapato', 'calcetín', 'camisa', 'pantalón',
+      'maleta', 'mochila', 'paraguas', 'llave', 'moneda', 'botella', 'vaso', 'plato'
+    ],
+    medium: [
+      'candelabro', 'jarrón', 'alfombra', 'cortina', 'cojín', 'edredón', 'almohada',
+      'percha', 'zapatero', 'estantería', 'cómoda', 'mesita', 'escritorio',
+      'archivador', 'rotulador', 'subrayador', 'grapadora', 'perforadora',
+      'calculadora', 'regla', 'compás', 'transportador', 'escuadra', 'cartabón',
+      'maletín', 'cartera', 'monedero', 'billetera', 'llavero', 'pañuelo'
+    ],
+    hard: [
+      'palmatoria', 'quinqué', 'farolillo', 'biombo', 'bargueño', 'canterano',
+      'papelera', 'cenicero', 'pisapapeles', 'abrecartas', 'sacacorchos',
+      'cascanueces', 'mondadientes', 'abrelatas', 'pelador', 'rallador',
+      'colador', 'espumadera', 'batidora de varillas', 'mortero', 'almirez',
+      'mancuernas', 'pesa rusa', 'banda elástica', 'esterilla', 'rodillo de foam'
+    ]
+  },
+
+  lugares: {
+    easy: [
+      'casa', 'escuela', 'hospital', 'parque', 'playa', 'montaña', 'río',
+      'lago', 'bosque', 'ciudad', 'pueblo', 'calle', 'plaza', 'tienda',
+      'supermercado', 'restaurante', 'bar', 'café', 'cine', 'teatro',
+      'museo', 'biblioteca', 'estadio', 'piscina', 'gimnasio', 'aeropuerto'
+    ],
+    medium: [
+      'catedral', 'mezquita', 'sinagoga', 'templo', 'monasterio', 'convento',
+      'castillo', 'palacio', 'fortaleza', 'faro', 'puerto', 'muelle', 'dársena',
+      'mercado', 'bazar', 'centro comercial', 'galería', 'ópera', 'auditorio',
+      'conservatorio', 'archivo', 'ayuntamiento', 'juzgado', 'comisaría',
+      'cuartel', 'embajada', 'consulado', 'aduana', 'frontera'
+    ],
+    hard: [
+      'mausoleo', 'necrópolis', 'cripta', 'osario', 'catacumba', 'hipogeo',
+      'zigurat', 'stupa', 'pagoda', 'monolito', 'dolmen', 'menhir', 'crómlech',
+      'acueducto', 'viaducto', 'puente levadizo', 'puente colgante', 'teleférico',
+      'funicular', 'cremallera', 'zoco', 'caravanserai', 'medina', 'kasbah',
+      'ghetto', 'favela', 'township', 'slum', 'shantytown', 'tenement'
+    ]
+  },
+
+  ciencia: {
+    easy: [
+      'átomo', 'molécula', 'célula', 'ADN', 'proteína', 'virus', 'bacteria',
+      'planeta', 'estrella', 'luna', 'sol', 'tierra', 'agua', 'fuego', 'aire',
+      'energía', 'fuerza', 'gravedad', 'luz', 'sonido', 'electricidad',
+      'magnetismo', 'temperatura', 'presión', 'volumen', 'masa', 'peso'
+    ],
+    medium: [
+      'mitocondria', 'ribosoma', 'cloroplasto', 'vacuola', 'nucleótido',
+      'aminoácido', 'enzima', 'hormona', 'neurona', 'sinapsis', 'gen',
+      'cromosoma', 'mutación', 'evolución', 'fotosíntesis', 'respiración',
+      'metabolismo', 'homeostasis', 'ecosistema', 'biodiversidad', 'cadena trófica',
+      'enlace covalente', 'enlace iónico', 'reacción química', 'catalizador'
+    ],
+    hard: [
+      'mitosis', 'meiosis', 'telómero', 'centrómero', 'epigenética', 'transcriptoma',
+      'proteoma', 'metaboloma', 'microbioma', 'CRISPR', 'quimera', 'transgénico',
+      'plásmido', 'bacteriófago', 'prion', 'retrovirus', 'ARN interferente',
+      'splicing alternativo', 'codón', 'anticodón', 'ribozima', 'aptámero',
+      'bosón de Higgs', 'quark', 'leptón', 'fermión', 'barión', 'mesón',
+      'fotón', 'gluón', 'neutrino', 'antimateria', 'materia oscura', 'energía oscura'
+    ]
+  }
+};
